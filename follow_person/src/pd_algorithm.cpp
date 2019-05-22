1cd1a05e6d19573e585a3b819726fcbc747afecc
Fernando <fergonzaramos@yahoo.es>
2019-05-22 18:20:08 +0200

    Last Changes

--- follow_person/src/pd_algorithm.cpp
+++ follow_person/src/pd_algorithm.cpp
@@ -3,18 +3,22 @@
 #include "follow_person/PersonFollowedData.h"
 #include "std_msgs/Int32.h"
 #include "std_msgs/Float32.h"
+#include "std_msgs/String.h"
 #include "sensor_msgs/CameraInfo.h"
+#include <bica/Component.h>
 #include <cmath>
+#include <string>
 
 const float Pi = 3.1416;
 
-class pdAlgorithm
+class pdAlgorithm: public bica::Component
 {
 
 private:
   ros::NodeHandle n_;
   ros::Subscriber sub_, info_sub_;
-  ros::Publisher speed_pub_;
+  ros::Publisher speed_pub_, talk_publisher_;
+  std_msgs::String data_;
 
   int width;
   float minDist, maxDist, Kp, Kd, prevErrorLinear, prevErrorAngular, maxV, maxW, prevV;
@@ -25,13 +29,14 @@ public:
     sub_ = n_.subscribe("/person_followed_data", 1, &pdAlgorithm::cb, this);
     info_sub_ = n_.subscribe("/camera/rgb/camera_info", 1, &pdAlgorithm::cb_info, this);
     speed_pub_ = n_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+    talk_publisher_ = n_.advertise<std_msgs::String>("/talk", 1);
 
     width = 0;
-    minDist = 700.0;
-    maxDist = 1000.0;
-    maxV = 0.6;
-    Kp = 0.4;
-    Kd = 0.6;
+    minDist = 900.0;
+    maxDist = 1200.0;
+    maxV = 0.5;
+    Kp = 0.3;
+    Kd = 0.7;
     prevV = 0;
     maxW = Pi / 2;
     prevErrorLinear = 0;
@@ -42,6 +47,9 @@ public:
   {
     //Calculo la salida del control proporcional
     if(d > maxDist){
+      std_msgs::String s;
+      //s.data = "Come closer, please";
+      //talk_publisher_.publish(s);
       d = maxDist;
     }
     float currentErrorLinear = (d - minDist) / (maxDist - minDist); //error entre 0 y 1
@@ -50,8 +58,11 @@ public:
 
     prevErrorLinear = currentErrorLinear;
     float v = output * maxV;
-    if(abs(v - prevV) >= maxV / 4.0){
-      v = v / 4.0;
+    if(abs(v - prevV) >= maxV / 8.0){
+      v = v / 8.0;
+    }
+    if(v < 0.0){
+      v = prevV;
     }
     prevV = v;
     return v;
@@ -70,18 +81,18 @@ public:
 
   void cb(const follow_person::PersonFollowedData::ConstPtr& msg)
   {
-    if(width != 0){
+    if(isActive()){
+      if(width != 0){
+        float dist = msg->dist;
+        int centralPixel = msg->centralPixel;
+        float v = pdAlgorithm::linear(dist);
+        float w = pdAlgorithm::angular(centralPixel);
+        geometry_msgs::Twist vel;
+        vel.linear.x = v;
+        vel.angular.z = w;
 
-      float dist = msg->dist;
-      int centralPixel = msg->centralPixel;
-      float v = pdAlgorithm::linear(dist);
-      float w = pdAlgorithm::angular(centralPixel);
-
-      geometry_msgs::Twist vel;
-      vel.linear.x = v;
-      vel.angular.z = w;
-
-      speed_pub_.publish(vel);
+        speed_pub_.publish(vel);
+      }
     }
   }
   void cb_info(const sensor_msgs::CameraInfo::ConstPtr& msg)
--- follow_person/src/person_followed_publisher.cpp
+++ follow_person/src/person_followed_publisher.cpp
@@ -9,84 +9,130 @@
 #include <vector>
 #include <algorithm>
 #include "follow_person/PersonFollowedData.h"
+#include "sensor_msgs/CameraInfo.h"
+#include <cmath>
+#include <ctime>
+#include "std_msgs/String.h"
+#include "bica/Component.h"
 
-const float maxDist = 2000.0;
+const float maxDist = 2100.0;
+const int MaxDistPixels = 170;
 
-ros::Publisher pub;
-std::vector<number_persons_recognition::BoundingBoxPerson> person_arr;
-
-float getDist(number_persons_recognition::BoundingBoxPerson p, const sensor_msgs::Image::ConstPtr& msg)
+class PersonData: public bica::Component
 {
-  float dist;
-  int width;
-  int height;
-  cv::Mat croppedImage;
-  std::vector<float> distVector;
-  std_msgs::Int32 size;
+private:
+  ros::NodeHandle n;
+  ros::Subscriber sub_image, sub_persons, info_sub, object_subscriber_;
+  ros::Publisher dataPub, talk_publisher;
+  std::vector<number_persons_recognition::BoundingBoxPerson> person_arr;
+  time_t currentTime = time(NULL);
+  time_t prevTime = time(NULL);
+  int prevCentralPixel;
+  std::string obj;
 
-  cv_bridge::CvImagePtr cv_image = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_32FC1);
-  width = p.xmax - p.xmin;
-  height = p.ymax - p.ymin;
-  cv::Mat ROI(cv_image->image, cv::Rect(p.xmin, p.ymin,
-              width, height));
-  ROI.copyTo(croppedImage);
-  dist = 0.0;
-  for(int j = 0; j < width; j++)
+public:
+  PersonData()
   {
-    for(int k = 0; k < height; k++)
-    {
-      dist = (float)croppedImage.at<float>(j, k);
-      distVector.push_back(dist);
-    }
+    dataPub = n.advertise<follow_person::PersonFollowedData>("/person_followed_data", 1);
+    talk_publisher = n.advertise<std_msgs::String>("/talk", 1);
+    info_sub = n.subscribe("/camera/rgb/camera_info", 1, &PersonData::cb_info, this);
+    sub_image = n.subscribe("/camera/depth/image_raw", 1, &PersonData::cb_image, this);
+    sub_persons = n.subscribe("/person_stimate", 1, &PersonData::cb_persons, this);
+    object_subscriber_ = n.subscribe("/orders", 1, &PersonData::objectCallback, this);
+    obj = "person";
   }
+  float getDist(number_persons_recognition::BoundingBoxPerson p, const sensor_msgs::Image::ConstPtr& msg)
+  {
+    float dist;
+    int width, height;
+    cv::Mat croppedImage;
+    std::vector<float> distVector;
+    std_msgs::Int32 size;
 
-  std::sort (distVector.begin(), distVector.end());
-  float med = distVector[(int)(distVector.size() / 2)];
-  return med;
-}
-
-int getCentralPixel(number_persons_recognition::BoundingBoxPerson p)
-{
-  return (int)((p.xmin + p.xmax) / 2);
-}
-
-void cb_persons(const number_persons_recognition::BoundingBoxPersonArray::ConstPtr& msg)
-{
-  person_arr = msg->persons_array;
-
-}
-
-void cb_image(const sensor_msgs::Image::ConstPtr& msg)
-{
-  if(person_arr.size() > 0){
-    float dist = getDist(person_arr[0], msg);
-    int centralPixel = getCentralPixel(person_arr[0]);
-    for(int i = 1; i < person_arr.size(); i++)
+    cv_bridge::CvImagePtr cv_image = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_32FC1);
+    width = p.xmax - p.xmin;
+    height = p.ymax - p.ymin;
+    cv::Mat ROI(cv_image->image, cv::Rect(p.xmin, p.ymin,
+                width, height));
+    ROI.copyTo(croppedImage);
+    dist = 0.0;
+    for(int j = 0; j < width; j++)
     {
-      int d = getDist(person_arr[i], msg);
-      if(d < dist){
-        dist = d;
-        centralPixel = getCentralPixel(person_arr[i]);
+      for(int k = 0; k < height; k++)
+      {
+        dist = (float)croppedImage.at<float>(j, k);
+        distVector.push_back(dist);
       }
     }
-    follow_person::PersonFollowedData data;
-    if(dist <= maxDist){
-      data.dist = dist;
-      data.centralPixel = centralPixel;
-      //Publico la distancia y el centro en x
-      pub.publish(data);
+
+    std::sort (distVector.begin(), distVector.end());
+    float med = distVector[(int)(distVector.size() / 2)];
+    return med;
+  }
+
+  int getCentralPixel(number_persons_recognition::BoundingBoxPerson p)
+  {
+    return (int)((p.xmin + p.xmax) / 2);
+  }
+
+  void cb_persons(const number_persons_recognition::BoundingBoxPersonArray::ConstPtr& msg)
+  {
+    person_arr = msg->persons_array;
+
+  }
+
+  void cb_image(const sensor_msgs::Image::ConstPtr& msg)
+  {
+    if(isActive()){
+      time_t currentTime = time(NULL);
+      if(person_arr.size() > 0){
+        float dist = getDist(person_arr[0], msg);
+        int centralPixel = getCentralPixel(person_arr[0]);
+        for(int i = 1; i < person_arr.size(); i++)
+        {
+          int d = getDist(person_arr[i], msg);
+          if(d < dist){
+            dist = d;
+            centralPixel = getCentralPixel(person_arr[i]);
+          }
+        }
+        if(obj == "person"){
+          if(dist < maxDist && abs(centralPixel - prevCentralPixel) <= MaxDistPixels){
+            follow_person::PersonFollowedData data;
+            prevCentralPixel = centralPixel;
+            data.dist = dist;
+            data.centralPixel = centralPixel;
+            //Publico la distancia y el centro en x
+            dataPub.publish(data);
+          }
+        }else{
+          follow_person::PersonFollowedData data;
+          prevCentralPixel = centralPixel;
+          data.dist = dist;
+          data.centralPixel = centralPixel;
+          //Publico la distancia y el centro en x
+          dataPub.publish(data);
+        }
+      }
     }
   }
 
-}
+  void cb_info(const sensor_msgs::CameraInfo::ConstPtr& msg)
+  {
+    int width = msg->width;
+    prevCentralPixel = (int)(width / 2);
+    info_sub.shutdown();
+  }
+
+  void objectCallback(const std_msgs::String::ConstPtr& msg)
+  {
+    obj = msg->data;
+  }
+};
 
 int main(int argc, char **argv) {
 
   ros::init(argc, argv, "Person_Followed_Publisher");
-  ros::NodeHandle n;
-
-  pub = n.advertise<follow_person::PersonFollowedData>("/person_followed_data", 1);
-  ros::Subscriber sub_image = n.subscribe("/camera/depth/image_raw", 1, cb_image);
-  ros::Subscriber sub_persons = n.subscribe("/person_stimate", 1, cb_persons);
+  PersonData personData;
   ros::spin();
 }
